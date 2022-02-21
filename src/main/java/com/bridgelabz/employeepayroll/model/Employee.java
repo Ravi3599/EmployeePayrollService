057e0e204f5e61fc9c4cfef456e95e397e6ababf
Ravi3599 <ravikantdukre2018@gmail.com>
2022-02-21 14:20:42 +0530

    UPDATED: used Lombok Library for auto generation

--- src/main/java/com/bridgelabz/employeepayroll/model/Employee.java
+++ src/main/java/com/bridgelabz/employeepayroll/model/Employee.java
@@ -6,11 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.aspectj.lang.annotation.RequiredTypes;
-
 import com.bridgelabz.employeepayroll.dto.EmployeeDTO;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Data
+@Getter
+@Setter
 public class Employee {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
